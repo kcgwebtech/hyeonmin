8b2aa1aef13b576ec1d63dd08944c6d87d249106
hyeonmin <hynmin1@naver.com>
2018-11-11 15:56:59 +0900

    4

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8888;
+
+const cookieParser = require('cookie-parser');
+const Token = require('./token');
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(Token.checkToken());
+
+app.get('/testAPI',(req,res) =>{
+    res.json({
+        data: {
+            message : 'API 테스트 성공',
+        }
+    });
+});
 
 app.use('/auth', require('./auth'));
 
