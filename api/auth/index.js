957cbb4eb6666fb91d79d6b433f3b7039b164c17
hyeonmin <hynmin1@naver.com>
2018-11-11 15:48:29 +0900

    3

--- api/auth/index.js
+++ api/auth/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/register', require('./register'));
+router.use('/login', require('./login'));
 
 module.exports = router;
